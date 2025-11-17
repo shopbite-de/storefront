fcb5b331a10371d332a1e6543ff241015121fb0b
Lirim Veliu <lirim@veliu.net>
2025-11-17 10:32:31 +0100

    Fixed category header

--- app/components/Category/Header.vue
+++ app/components/Category/Header.vue
@@ -7,7 +7,10 @@ defineProps<{
 </script>
 
 <template>
-  <div class="relative mb-4 mt-8 h-40 w-full overflow-hidden rounded-lg">
+  <div
+    v-if="category.media?.url"
+    class="relative mb-4 mt-8 h-40 w-full overflow-hidden rounded-lg"
+  >
     <NuxtImg
       v-if="category.media?.url"
       :src="category.media.url"
@@ -16,14 +19,35 @@ defineProps<{
       alt="Pizza Kategorie"
       placeholder
     />
-    <div class="absolute inset-0 bg-gradient-to-t from-black/50 to-black/10" />
+    <div
+      v-if="category.media?.url"
+      class="absolute inset-0 bg-gradient-to-t from-black/50 to-black/10"
+    />
+
+    <div v-else class="absolute inset-0 bg-primary" />
+
     <div class="relative z-10 p-4">
-      <h2 class="text-3xl md:text-4xl text-white drop-shadow">
+      <h2
+        class="text-pretty font-semibold text-3xl md:text-4xl text-white drop-shadow"
+      >
         {{ category.name }}
       </h2>
-      <h3 v-if="category.description" class="text-white/90">
+      <h3
+        v-if="category.description"
+        class="text-white/90 text-[15px] text-pretty mt-1"
+      >
         {{ category.description }}
       </h3>
     </div>
   </div>
+  <UPageCard
+    v-else
+    :title="category.translated.name ?? category.name"
+    :description="category.translated.description ?? category.description"
+    variant="soft"
+    class="my-4"
+    :ui="{
+      title: 'text-3xl md:text-4xl',
+    }"
+  />
 </template>
