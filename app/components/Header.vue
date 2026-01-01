b1fb516f62390566cd32b4c7b4106ba72dd03200
Lirim Veliu <lirim@veliu.net>
2026-01-01 22:34:12 +0100

    Replaced svg logos with png logos

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -84,8 +84,9 @@ const cartQuickViewOpen = ref(false);
     <template #title>
       <NuxtLink to="/" class="-m-1.5 p-1.5">
         <span class="sr-only">{{ siteName }}</span>
-        <NuxtImg
-          src="/light/Logo.svg"
+        <UColorModeImage
+          light="/light/Logo.png"
+          dark="/dark/Logo.png"
           class="h-12 w-auto"
         />
       </NuxtLink>
